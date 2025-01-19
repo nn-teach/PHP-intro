27ae2c392cb5b26724355f7cdc495d401ac9f9cd
Florian Rodriguez <florian@negative-network.com>
2025-01-19 15:37:08 +0100

    Création de sa première page web avec PHP et gestion de multi pages

---
+++ about.php
@@ -0,0 +1,4 @@
+<?php include 'header.php'; ?> 
+   <h1>À propos de nous</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi dignissimos repellendus rem deserunt, doloremque dolorem voluptate, sed eveniet distinctio vero numquam neque similique deleniti pariatur fugiat maxime dicta provident magni!</p>
+<?php include 'footer.php'; ?>
---
+++ contact.php
@@ -0,0 +1,4 @@
+<?php include 'header.php'; ?>
+<h1>Contact</h1>
+<p>Vous pouvez me contacter à test@test.com</p>
+<?php include 'footer.php'; ?>

44039850a6abdafa837fbde5a9e9d488615ea461
Florian Rodriguez <florian@negative-network.com>
2025-01-20 01:19:07 +0100

    CRUD - Final

--- blog.php
+++ blog.php
@@ -2,21 +2,21 @@
 <?php include 'header.php'; ?>
 <h1>Mini-Blog</h1>
 
-<a href="create.php">Créer un nouvel article</a>
-
 <?php
 echo "<h2>Articles publiés</h2>";
+echo '<section class="articles"><a href="create.php" class="new button">Créer un nouvel article</a>';
 foreach ($articles as $article) {
     echo "<article>";
     echo "<h3><a href=read.php?articleId=". $article->id. ">" . htmlspecialchars($article->titre) . "</a></h3>";
     echo "<p>" . nl2br(htmlspecialchars($article->contenu)) . "</p>";
     echo "<div>";
-    echo "<a href=update.php?articleId=". $article->id.">modifier</a>";
+    echo "<a href=update.php?articleId=". $article->id." class='button'>modifier</a>";
     echo    '<form method="post" action="blog.php">
             <input type="hidden" name="articleId" value="'.$article->id.'">
-            <input type="submit" value="Supprimer" name="supprimer_article">';
+            <input type="submit" value="Supprimer" name="supprimer_article" class="button">';
     echo "</div>";
     echo "</article>";
 }
+echo "</section>";
 ?>
 <?php include 'footer.php'; ?>
